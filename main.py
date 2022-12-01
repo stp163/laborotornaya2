8befdb2654203f20ac37b6ca2fc0405e73523ec5
stp163 <118365213+stp163@users.noreply.github.com>
2022-12-01 15:35:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+s_city ="Moscow,RU"
+appid ="5930f4c7fd9ece22ee82f1e6009c43dd"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                   params={'q':s_city,'units':'metric','lang':'ru','APPID': appid})
+data = res.json()
+print("Город:", s_city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': s_city,'units':'metric','lang':'ru','APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'],"> \r\nТемпература<",'{0:+3.0f}'.format(i['main']['temp']),"> \r\nПогодныеусловия <",i['weather'][0]['description'],">")
+    print("____________________________")
